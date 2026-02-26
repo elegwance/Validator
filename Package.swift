c1a6271caff7ac3d89c4dd3a5bcfe72442b05059
JaeKwang Lee <me@elegwance.com>
2026-02-26 17:59:22 +0900

    Simplify Package.swift by removing unused sections

--- Package.swift
+++ Package.swift
@@ -3,25 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Validator",
-    platforms: [
-        .iOS(.v13), .macOS(.v10_15)
-    ],
-    products: [
-        .library(
-            name: "Validator",
-            targets: ["Validator"]
-        ),
-    ],
-    targets: [
-        .target(
-            name: "Validator",
-            path: "Validator/Source", // 실제 소스 코드 경로에 맞게 수정 필요
-            exclude: ["Info.plist"]
-        ),
-        .testTarget(
-            name: "ValidatorTests",
-            dependencies: ["Validator"],
-            path: "ValidatorTests"
-        ),
-    ]
+    platforms: [.iOS(.v13)],
+    products: [.library(name: "Validator", targets: ["Validator"])],
+    targets: [.target(name: "Validator", path: "Validator/Sources")]
 )
